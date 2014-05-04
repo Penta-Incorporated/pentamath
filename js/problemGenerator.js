4dea6a76ad1d8fbd973f047b8486b7abab7450b8
Bradley Garagan <bradleygaragan@live.ca>
2014-05-03 21:42:06 -0300

    css

---
+++ js/problemGenerator.js
@@ -0,0 +1,41 @@
+var operators = ['+','/','*','-'];
+
+function rand_int(a,b){
+	return Math.round((Math.random() * (b - a)) + a);
+}
+
+function lengthOfLast(problem){
+	var len = 0;
+	for(var i=problem.length-1;i>=0;i--){
+		if (! operators.contains(problem[i])
+			len++;
+		else break;
+	}
+
+	return len;
+}
+
+function easyDiv(){
+	result = new Array();
+	//divisor
+	result[0] = rand_int(2,14);
+	//dividend
+	result[1] = result[0] * rand_int(2,8);
+
+	return result;
+}
+
+
+function generateProblem(rating){
+	
+	var problem = '';
+	var numOps = (rating + 600)/1000;
+	var current_ops = new Array();
+
+	for(var i=0; i<numOps; i++){
+		current_ops[i] = operators[rand_int(0,3)];
+	}
+
+	
+
+}
