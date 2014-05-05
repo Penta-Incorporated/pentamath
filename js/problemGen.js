4d58f317ecad30f2e7ad5e6ff0a5076825dbece4
Bradley Garagan <bradleygaragan@live.ca>
2014-05-05 18:46:05 -0300

    update

--- js/problemGen.js
+++ js/problemGen.js
@@ -1,66 +1,64 @@
-/*Takes the persons elo
-start at 1000 elo
-10 problem mode 50 problem mode and so on
-so percentage of problems solved would have 50 subtracted from it 
-and anything below a 50 would make you lose elo anything above would make you gain elo
-we would have a 50 multiplier on it*/
+function rand_int(a,b){
+	return Math.round((Math.random() * (b - a)) + a);
+};
+function check(operation){
+	for(var q = 0; q < operation.length; q++){
+			if(operation[q] === "*" || operation[q] === "/"){
+				return true;
+			}
+	}
 
-//Code:
+	return false;
+};
+
 var createProblem = function(elo){
 
-    var problem;
-    var operationArray = ['+','-','*','/'];
-    var count = new Array();
-    var operation = new Array();
-    var modifier = Math.floor(elo/400);
-    
-    if(modifier > 3 ){
-        modifier = 3;
-    }
-    for(var q = 0; q < modifier; q++){
-        
-        count[q] = Math.floor(Math.random()*(elo/10))+1;
-        
-    }//Create the numbers
-    modifier = Math.floor(elo/600);
-    if(modifier > 2 ){
-        modifier = 2;
-    }
-    for(var q = 0; q < modifier; q++){
-        var y = 0;
-        if(count[q]%10 === 0){
-            y = 4;
-        }
-        else{
-            y = 2;
-        }
-         operation[q] = operationArray[Math.floor(Math.random()*y)];
-         if(operation[q] === "*" || operation[q] === "/"){
-             count[q+1] = Math.floor(Math.random()*10)+2;
-         }
-         if(operation[q] === "/"){
-             var temp = count[q-1]/count[q+1];
-             while(temp !== Math.round(temp)){
-                 count[q+1] = Math.floor(Math.random()*10);
-                 temp = count[q-1]/count[q+1];
-             }
-         }
-     
-    }//Create the operation
-    for(var q = 0; q < operation.length+1; q++){
-        if(q === 0){
-           problem = count[q];
-        }
-        else{
-            problem += count[q];
-        }
-        if(q > operation.length-1){
-        }
-        else{
-            problem += operation[q];
-        }
-        
-    }//Add the operation and the number together
-    return problem;
-    
-}
+	var problem;
+	var operationArray = ['+','-','*','/'];
+	var count = new Array();
+	var operation = new Array();
+	var modifier = Math.floor(elo/1200);
+	if (modifier < 1)
+		modifier = 1;
+
+	if((modifier+1) > 1 ){
+	    modifier = rand_int(1,modifier);
+	}
+	for(var q = 0; q < (modifier + 1); q++){
+
+	    count[q] = rand_int(1,(elo/10));
+
+	}//Create the numbers
+	for(var q = 0; q < modifier; q++){
+			 if(check(operation)){
+				operation[q] = operationArray[rand_int(0,1)];
+			 }
+			 else{
+				operation[q] = operationArray[rand_int(0,3)];
+			 }
+	     if(operation[q] === "*"){
+	         count[q+1] = Math.round(count[q] / (elo/100));
+	     }
+	     if(operation[q] === "/"){
+		count[q+1] = Math.round(count[q+1]/8);
+		count[q] = count[q+1] * rand_int(2,10);
+	     }
+
+	}//Create the operation
+	for(var q = 0; q < operation.length+1; q++){
+	    if(q === 0){
+	       problem = count[q];
+	    }
+	    else{
+	        problem += count[q];
+	    }
+	    if(q > operation.length-1){
+	    }
+	    else{
+	        problem += operation[q];
+	    }
+
+	}//Add the operation and the number together
+	return problem;
+
+};
